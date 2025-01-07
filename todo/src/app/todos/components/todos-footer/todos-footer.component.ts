3ccf91532029cd0e44d5f292ffa7e683a7ec0950
Ricki W. <ricki.weigel@gmx.de>
2025-01-07 22:39:19 +0100

    feat: implement footer with filter functionality and active todo count

--- todo/src/app/todos/components/todos-footer/todos-footer.component.ts
+++ todo/src/app/todos/components/todos-footer/todos-footer.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { TodosService } from '../../services/todos.service';
+import { FilterEnum } from '../../types/filter.enum';
 
 @Component({
   selector: 'app-todos-footer',
   imports: [],
   templateUrl: './todos-footer.component.html',
-  styleUrl: './todos-footer.component.scss'
+  styleUrl: './todos-footer.component.scss',
 })
 export class FooterComponent {
+  todosService = inject(TodosService);
+  filterSig = this.todosService.filterSig;
+  filterEnum = FilterEnum;
 
+  activeCount = computed(() => {
+    return this.todosService.todosSig().filter((todo) => !todo.isCompleted).length;
+  });
+
+  changeFilter(event: Event, filterName: FilterEnum): void {
+    event.preventDefault();
+    this.todosService.changeFilter(filterName);
+  }
 }
