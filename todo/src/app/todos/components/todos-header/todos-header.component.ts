2651f18ebb1409d251f733e10083df1d0af2edd9
Ricki W. <ricki.weigel@gmx.de>
2025-01-07 14:56:28 +0100

    feat: Add input handling and addTodo method in HeaderComponent for creating new todos

--- todo/src/app/todos/components/todos-header/todos-header.component.ts
+++ todo/src/app/todos/components/todos-header/todos-header.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TodosService } from '../../services/todos.service';
 
 @Component({
   selector: 'app-todos-header',
   imports: [],
   templateUrl: './todos-header.component.html',
-  styleUrl: './todos-header.component.scss'
+  styleUrl: './todos-header.component.scss',
 })
 export class HeaderComponent {
+  todosService = inject(TodosService);
+  text: string = '';
 
+  changeText(event: Event):void{
+    const target = event.target as HTMLInputElement;
+    this.text = target.value;
+  }
+
+  addTodo():void{
+    this.todosService.addTodo(this.text);
+    this.text = '';
+  }
 }
