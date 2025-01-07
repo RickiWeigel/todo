1d74f3963b892bb00ac1e45835be3ba383ee099b
Ricki W. <ricki.weigel@gmx.de>
2025-01-07 14:54:55 +0100

    feat: Add filtered todo list with 'visibleTodos' computed property and display in HTML

--- todo/src/app/todos/components/todos-main/todos-main.component.ts
+++ todo/src/app/todos/components/todos-main/todos-main.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { TodosService } from '../../services/todos.service';
+import { FilterEnum } from '../../types/filter.enum';
 
 @Component({
   selector: 'app-todos-main',
   imports: [],
   templateUrl: './todos-main.component.html',
-  styleUrl: './todos-main.component.scss'
+  styleUrl: './todos-main.component.scss',
 })
 export class MainComponent {
+  todosService = inject(TodosService);
 
+  visibleTodos = computed(() => {
+    const todos = this.todosService.todosSig();
+    const filter = this.todosService.filterSig();
+
+    if (filter === FilterEnum.active) {
+      return todos.filter((todo) => !todo.isCompleted);
+    } else if (filter === FilterEnum.completed) {
+      return todos.filter((todo) => todo.isCompleted);
+    }
+    return todos;
+  });
 }
